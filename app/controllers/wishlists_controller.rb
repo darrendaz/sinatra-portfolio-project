3421c82aa9ce75c7e34d4ad510dfd8476e640278
darrendaz <darren.lou@gmail.com>
2019-05-30 14:17:54 -0700

    WIP building routes for get /wishlists & /new and post /wishlists

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,6 +1,19 @@
 class WishlistsController < ApplicationController
     get '/wishlists' do
-        "wishlists"
+        binding.pry
+        @public_wishlists = Wishlist.all.where(private: 0)
+        erb :"wishlists/show"
+    end
+
+    post '/wishlists' do
+        wishlist = Wishlist.create(params)
+        wishlist.user_id = current_user.id
+        wishlist.save
+        redirect "/wishlists"
+    end
+    
+    get '/wishlists/new' do
+        erb :"/wishlists/new"
     end
 
 end
