4423c555dc18d466e075e652dcc6d4b9040301a7
darrendaz <darren.lou@gmail.com>
2019-05-30 11:53:55 -0700

    implemented use of current_user method on login and show .erb pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,37 @@ class UsersController < ApplicationController
         if params[:name] == "" || params[:username] == "" || params[:password] == ""
             redirect "/signup"
         else
-            binding.pry
-            @user = User.create(params)
-            session[:user_id] = @user.id
-            redirect "/users/:id"
+            user = User.create(params)
+            session[:user_id] = user.id
+            redirect "/users/#{user.id}"
         end
     end
 
-    get '/users/:id' do
-        "howdy"
+    get "/users/:id" do
+        if logged_in?
+            erb :"users/show"
+        else
+            redirect "/"
+        end
     end
 
     get '/logout' do
         session.clear
+        redirect "/"
+    end
+
+    get '/login' do
+        erb :"users/login"
+    end
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/"
+        else
+            redirect "/login"
+        end
     end
 
 end
