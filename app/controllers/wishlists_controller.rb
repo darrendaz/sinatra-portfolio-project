c21be022bfbe7474ee3d7f45e98b4840133e326c
darrendaz <darren.lou@gmail.com>
2019-05-30 16:56:15 -0700

    added edit erb file

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,19 +4,31 @@ class WishlistsController < ApplicationController
         erb :"wishlists/index"
     end
 
+    
     post '/wishlists' do
         wishlist = Wishlist.create(params)
         wishlist.user_id = current_user.id
         wishlist.save
         redirect "/#{current_user.id}/wishlists"
     end
-
+    
     get '/:id/wishlists'do
         erb :"wishlists/user-wishlists"
     end
-    
+
     get '/wishlists/new' do
-        erb :"/wishlists/new"
+        erb :"wishlists/new"
+    end
+    
+    get '/wishlists/:id' do
+        @wishlist = current_user.wishlists.find(params[:id])
+        erb :"wishlists/show"
+    end
+
+    get '/wishlists/:id/edit' do
+        binding.pry
+        @wishlist = current_user.wishlists.find(params[:id])
+        erb :"wishlists/edit"
     end
 
 end
