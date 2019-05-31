2ce7229706f5b940a46b45f8b05c674269ac9e20
darrendaz <darren.lou@gmail.com>
2019-05-31 10:58:17 -0700

    WIP experimental code for displaying and updating selected items already in wishlist on wishlist edit erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,20 @@
 class ItemsController < ApplicationController
-    get '/items' do
-        erb :"items/index"
-    end
+	get '/items' do
+		@items = Item.all
+		erb :"items/index"
+	end
 
-    post '/items' do
-        item = Item.create(params)
-        redirect "/items"
-    end
-    
-    get '/items/new' do
-        erb :"items/new"
-    end
-    
-    get '/items/:id' do
-        @item = Item.find(params[:id])
-        erb :"items/show"
-    end
-    
-
-
+	post '/items' do
+		item = Item.create(params)
+		redirect "/items"
+	end
+	
+	get '/items/new' do
+		erb :"items/new"
+	end
+	
+	get '/items/:id' do
+		@item = Item.find(params[:id])
+		erb :"items/show"
+	end
 end
--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,46 +1,73 @@
 class WishlistsController < ApplicationController
-    get '/wishlists' do
-        @public_wishlists = Wishlist.all.where(private: 0)
-        erb :"wishlists/index"
-    end
+	get '/wishlists' do
+			@public_wishlists = Wishlist.all.where(private: 0)
+			erb :"wishlists/index"
+	end
 
-    
-    post '/wishlists' do
-        wishlist = Wishlist.create(params)
-        wishlist.user_id = current_user.id
-        wishlist.save
-        redirect "/#{current_user.id}/wishlists"
-    end
-    
-    get '/:id/wishlists'do
-        erb :"wishlists/user-wishlists"
-    end
+	post '/wishlists' do
+		binding.pry
+		# if !params["item"]["name"].empty?
+		# 	@wishlist.items << Item.create(name: params["item"]["name"])
+		# end
 
-    get '/wishlists/new' do
-        erb :"wishlists/new"
-    end
-    
-    get '/wishlists/:id' do
-        @wishlist = current_user.wishlists.find(params[:id])
-        erb :"wishlists/show"
-    end
+		Wishlist.find(@wishlist.id).update(params[:wishlist][:items])
+		redirect "wishlists/#{@wishlist.id}"
+	end
 
-    patch '/wishlists/:id' do
-        wishlist = current_user.wishlists.find(params[:id])
+	# post '/wishlists' do
+	# 		wishlist = Wishlist.create(params)
+	# 		wishlist.user_id = current_user.id
+	# 		wishlist.save
+	# 		redirect "/#{current_user.id}/wishlists"
+	# end
+	
+	get '/:id/wishlists'do
+			erb :"wishlists/user-wishlists"
+	end
 
-        wishlist.update(name: params[:name], private: params[:private])
-        redirect "/wishlists/#{wishlist.id}"
-    end
+	get '/wishlists/new' do
+			erb :"wishlists/new"
+	end
+	
+	get '/wishlists/:id' do
+			binding.pry
+			@wishlist = current_user.wishlists.find(params[:id])
+			erb :"wishlists/show"
+	end
 
-    get '/wishlists/:id/edit' do
-        @wishlist = current_user.wishlists.find(params[:id])
-        erb :"wishlists/edit"
+	patch '/wishlists/:id' do
+    ####### bug fix
+    if !params[:wishlist].keys.include?("item_ids")
+    	params[:wishlist]["item_ids"] = []
     end
+    #######
+ 
+    @wishlist = Wishlist.find(params[:id])
+    @wishlist.update(params["wishlist"])
+			if !params["item"]["name"].empty?
+				@wishlist.items << Item.create(name: params["item"]["name"])
+			end
+    redirect "wishlists/#{@wishlist.id}"
+	end
 
-    delete '/wishlists/:id/delete' do
-        @wishlist = current_user.wishlists.find(params[:id])
-        @wishlist.delete
-        redirect "/#{current_user.id}/wishlists"
-    end
+	# patch '/wishlists/:id' do
+	# 		wishlist = current_user.wishlists.find(params[:id])
+
+	# 		wishlist.update(name: params[:name], private: params[:private])
+	# 		redirect "/wishlists/#{wishlist.id}"
+	# end
+
+	get '/wishlists/:id/edit' do
+			@wishlist = current_user.wishlists.find(params[:id])
+
+			@items = Item.all
+			erb :"wishlists/edit"
+	end
+
+	delete '/wishlists/:id/delete' do
+			@wishlist = current_user.wishlists.find(params[:id])
+			@wishlist.delete
+			redirect "/#{current_user.id}/wishlists"
+	end
 
 end
