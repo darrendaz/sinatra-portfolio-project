1ca5dced3657c8a61b60b09b2b6abf67ad6f6e7a
darrendaz <darren.lou@gmail.com>
2019-05-30 19:54:04 -0700

    WIP /items/:id route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,16 @@ class ItemsController < ApplicationController
         item = Item.create(params)
         redirect "/items"
     end
-
+    
     get '/items/new' do
         erb :"items/new"
     end
+    
+    get '/items/:id' do
+        @item = Item.find(params[:id])
+        erb :"items/show"
+    end
+    
+
 
 end
